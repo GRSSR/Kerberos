6b4a37b0b55a968c6a6bc5efbefc62026172eea1
Taran Moltu <taranmoltu@gmail.com>
2014-10-19 00:14:52 +0100

    Updates!
    Implemented the hello() method so that multiple modems can be used,
    added a debug option and added the ability to handle multiple doors via
    config options

--- doorClient.lua
+++ doorClient.lua
@@ -1,21 +1,82 @@
 os.loadAPI("api/sovietProtocol")
+os.loadAPI("api/arg")
+local args = arg.parseArgs({...})
 
-doorID = os.getComputerID()
 local PROTOCOL_CHANNEL = 1
-sovietProtocol.setDebugLevel(9)
-local laika = sovietProtocol.Protocol:new("laika", PROTOCOL_CHANNEL, doorID)
+local DEBUG_LEVEL = 0
 
-local function openDoor()
-	redstone.setOutput("bottom", true)
+local CONFIG = {}
+
+CONFIG.id = os.getComputerID()
+CONFIG.doors = {}
+
+if args.v then
+	print("debug mode")
+	DEBUG_LEVEL = 9
 end
 
-local function closeDoor()
-	redstone.setOutput("bottom", false)
+function getConfig(file)
+
+	f = io.open(file, "r")
+	if f then
+		for line in f:lines() do
+			split = redString.split(line)
+			CONFIG.doors[split[1]] = {
+				side = split[2]}
+			if DEBUG_LEVEL > 3 then
+				print("Loaded door with id "..split[1].." which outputs on side "..split[2])
+			end
+		end
+		f:close()
+		return true
+	else
+		return false
+	end
 end
 
-function openCheck(id)
+if not getConfig("laika.conf") then
+	print("No config detected, loading simple config.")
+	CONFIG.doors[os.getComputerID()] = {
+		side = "all"
+	}
+end
+
+sovietProtocol.setDebugLevel(DEBUG_LEVEL)
+
+local laika  = nil
+for side, modem in pairs(sovietProtocol.findModems()) do 
+	local possible = sovietProtocol.Protocol:new("laika", PROTOCOL_CHANNEL, CONFIG.id, side)
+	if possible:hello() then
+		laika = possible
+		break;
+	else
+		possible:tearDown()
+	end
+end
+
+local function openDoor(side)
+	if side == "all" then
+		for k, side in pairs(redstone.getSides()) do
+			redstone.setOutput(side, true)
+		end
+	else
+		redstone.setOutput(side, true)
+	end
+end
+
+local function closeDoor(side)
+	if side == "all" then
+		for k, side in pairs(redstone.getSides()) do
+			redstone.setOutput(side, false)
+		end
+	else
+		redstone.setOutput(side, false)
+	end
+end
+
+function openCheck(id, door)
 	print("sending check")
-	laika:send("can_open", doorID, id)
+	laika:send("can_open", door, id)
 	print("waiting for response")
 	local sender, response = laika:listen()
 	if response.method == "door_open" and response.body == "true" then
@@ -24,33 +85,32 @@ function openCheck(id)
 		return false
 	end
 end
-
-local function checkID(id)
-	return openCheck(id)
-end
-
+local id = ""
 while true do
 	id = ""
 	local event, drive = os.pullEvent("disk")
 	diskRoot = disk.getMountPath(drive)
 	idFile = fs.combine(diskRoot, "ID")
+
 	if fs.exists(idFile) then 
 		f = io.open(idFile, "r")
 		for line in f:lines() do
 			id = id..line
 		end
 		f:close()
-		print(id)
 	else
 		print("invalid disk")
 	end
+
 	disk.eject(drive)
 
-	if checkID(id) then
-			openDoor()
-			sleep(5)
-			closeDoor()
-	else
-		print("invalid ID")
+	for doorID, door in pairs(CONFIG.doors) do
+		if openCheck(id, doorID) then
+				openDoor(door.side)
+				sleep(5)
+				closeDoor(door.side)
+		else
+			print("invalid ID for door "..doorID)
+		end
 	end
 end
